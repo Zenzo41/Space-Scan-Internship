76a719f294001fd79e754ba1e85998b8c6b330cb
Sonam Syangbo <syangbo.sonam88@gmail.com>
2023-09-20 08:40:37 +0545

    random password generator

---
+++ task2.py
@@ -0,0 +1,13 @@
+# random password generator
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the desired password length: "))
+    random_password = generate_password(password_length)
+    print("Your random password is:", random_password)
